f6adce02600a0a96e648923663e49a83f5f498cc
katebron <kate.bronstad@tufts.edu>
2016-08-31 20:26:38 -0400

    click & set current location

--- js/app.js
+++ js/app.js
@@ -4,6 +4,13 @@ initialLocations = [
     addy: 'located here',
     description: 'here is a description'
   },
+  {
+    title: 'wilson high school',
+    addy: '1151 SW Vermont St, Portland, OR 97219 ',
+    description: 'high school near old house',
+    latitude: '45.4771954',
+    longitude: '-122.6920507'
+  },
 ]
 
 var Location = function(data) {
@@ -12,9 +19,14 @@ var Location = function(data) {
   this.title = ko.observable(data.title);
   this.addy = ko.observable(data.addy);
   this.description = ko.observable(data.description);
+  this.latitude = ko.observable(data.latitude);
+  this.longitude = ko.observable(data.longitude);
   //this.imgAttribution = ko.observable(data.imgAttribution);
 
   //this.nicknames = ko.observableArray(data.nicknames);
+  
+
+
 
 }
 
@@ -28,9 +40,21 @@ var ViewModel = function() {
   });
   // use self whenever you want to get to the outer "this" in a
   // nested function like below. 
-  this.currentLocation = ko.observable(this.locationList()[0]);
+  this.currentLocation = ko.observable(this.locationList()[1]);
+  
+  this.setCurrentLocation = function(clickedLocation) {
+    self.currentLocation(clickedLocation);
+  }
 
- 
+  self.returnCoordinates = ko.computed(function() {
+       var coordinates = self.currentLocation().latitude() + " by " + self.currentLocation().longitude();   
+       console.log("this is latitude " + self.currentLocation().latitude());  
+       return coordinates;
+
+    });    
+  
+
+
   //this.incrementCounter = function (clickedCat) {
    // self.currentCat().clickCount(self.currentCat().clickCount() + 1);
    //};
